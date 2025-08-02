eea0323449c214ad56f89a53927803e665fb4867
lgwk42 <luisgunkim@gmail.com>
2025-08-02 17:13:51 +0900

    feat: project init

---
+++ src/test/java/com/spring/lukeapiserver/LukeApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lukeapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukeApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
